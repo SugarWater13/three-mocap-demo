6ceb42d51e68a1ec77f993336a9c61a7c74b826d
SugarWater13 <mr.ajselig@gmail.com>
2021-08-19 16:44:01 -0600

    fix: do things simpler

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
-// Test import of a JavaScript module
-import { example } from '@/js/example'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { BVHLoader } from 'three/examples/jsm/loaders/BVHLoader'
 
-// Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+const clock = new THREE.Clock()
 
-// Test import of styles
-import '@/styles/index.scss'
+let camera, controls, scene, renderer
+let mixer, skeletonHelper
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+init()
+animate()
 
-const heading = document.createElement('h1')
-heading.textContent = example()
+const loader = new BVHLoader()
+loader.load('bvh/01/01_01.bvh', function(result) {
+    skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0])
+    skeletonHelper.skeleton = result.skeleton // allow animation mixer to bind to THREE.SkeletonHelper directly
 
-const app = document.querySelector('#root')
-app.append(logo, heading)
+    const boneContainer = new THREE.Group()
+    boneContainer.add(result.skeleton.bones[0])
+
+    scene.add(skeletonHelper)
+    scene.add(boneContainer)
+
+    // play animation
+    mixer = new THREE.AnimationMixer(skeletonHelper)
+    mixer.clipAction(result.clip).setEffectiveWeight(1.0).play()
+})
+
+function init() {
+    camera = new THREE.PerspectiveCamera(
+        60,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+    camera.position.set(0, 200, 300)
+
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xeeeeee)
+
+    scene.add(new THREE.GridHelper(400, 10))
+
+    // renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.minDistance = 300
+    controls.maxDistance = 700
+
+    window.addEventListener('resize', onWindowResize)
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    const delta = clock.getDelta()
+
+    if (mixer) mixer.update(delta)
+
+    renderer.render(scene, camera)
+}
